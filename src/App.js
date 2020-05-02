f5f5c68cd3e78a55e5696a2b0508a08497971928
unknown <andrelopessfla@gmail.com>
2020-05-02 20:50:39 -0300

    criado o primeiro grafico

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Chart from './components/chart/chart';
+
+
+function App() {
+  return (
+    <div >
+     <Chart/>
+     
+    </div>
+  );
+}
+
+export default App;
